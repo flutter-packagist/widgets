77e70999b26710d951740bc2a64e50f89e2a47a1
LioLin <windows7lake@gmail.com>
2025-09-16 16:37:57 +0800

    feat: 图片带标签支持boxfit contain

--- example/lib/label_image_demo.dart
+++ example/lib/label_image_demo.dart
@@ -10,6 +10,81 @@ class LabelImageDemo extends StatefulWidget {
 }
 
 class _LabelImageDemoState extends State<LabelImageDemo> {
+  List<ImageBlock> images = [
+    ImageBlock(
+      image:
+          "https://cp4.100.com.tw/images/works/202408/20/api_2131628_1724146396_Uw3N5p3p0l.jpg!10961t1500-v2.jpg",
+      fit: BoxFit.contain,
+      labels: [
+        LabelItem(name: "吊柜", percentX: 0.75, percentY: 0.4),
+        LabelItem(name: "艺术画", percentX: 0.45, percentY: 0.5),
+        LabelItem(name: "沙发", percentX: 0.35, percentY: 0.7),
+      ],
+    ),
+    ImageBlock(
+      image:
+          "https://cp4.100.com.tw/images/works/202408/20/api_2131628_1724146315_T14buIm3xY.jpg!10961t1500-v2.jpg",
+      fit: BoxFit.contain,
+      labels: [
+        LabelItem(name: "吊灯", percentX: 0.55, percentY: 0.17),
+        LabelItem(name: "花瓶", percentX: 0.55, percentY: 0.5),
+        LabelItem(name: "柜子", percentX: 0.4, percentY: 0.7),
+      ],
+    ),
+    ImageBlock(
+      image:
+          "https://cp4.100.com.tw/images/works/202505/16/api_2012032_1747362772_TBECTIDzJt.jpg!10309t1500-v2.jpg",
+      fit: BoxFit.cover,
+      labels: [
+        LabelItem(name: "吊頂燈帶", percentX: 0.3, percentY: 0.3),
+        LabelItem(name: "電視墻", percentX: 0.4, percentY: 0.5),
+        LabelItem(name: "沙發", percentX: 0.7, percentY: 0.7),
+      ],
+    ),
+    ImageBlock(
+      image:
+          "https://cp4.100.com.tw/images/works/202301/03/api_2114251_1672736564_sk5rc5Iy49.jpg!t1000.webp",
+      fit: BoxFit.cover,
+      labels: [
+        LabelItem(name: "壁櫃", percentX: 0.53, percentY: 0.47),
+        LabelItem(name: "冰箱", percentX: 0.69, percentY: 0.6),
+        LabelItem(name: "燃氣灶", percentX: 0.2, percentY: 0.8),
+      ],
+    ),
+  ];
+
+  List<Widget> widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < images.length; i++) {
+      widgets.addAll([
+        WrapperLabelImage(
+          imageUrl: images[i].image,
+          labels: images[i].labels,
+          fit: images[i].fit,
+        ),
+        const SizedBox(height: 20),
+        WrapperLabelImage(
+          height: 200,
+          imageUrl: images[i].image,
+          labels: images[i].labels,
+          fit: images[i].fit,
+        ),
+        const SizedBox(height: 20),
+        WrapperLabelImage(
+          width: images[i].fit == BoxFit.cover ? 300 : 200,
+          height: 250,
+          imageUrl: images[i].image,
+          labels: images[i].labels,
+          fit: images[i].fit,
+        ),
+        const SizedBox(height: 20),
+      ]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,77 +93,22 @@ class _LabelImageDemoState extends State<LabelImageDemo> {
         backgroundColor: Colors.white,
       ),
       body: SingleChildScrollView(
-        child: Column(children: [
-          WrapperLabelImage(
-            imageUrl:
-                "https://cp4.100.com.tw/images/works/202505/16/api_2012032_1747362772_TBECTIDzJt.jpg!10309t1500-v2.jpg",
-            labels: [
-              LabelItem(name: "吊頂燈帶", percentX: 0.3, percentY: 0.3),
-              LabelItem(name: "電視墻", percentX: 0.4, percentY: 0.5),
-              LabelItem(name: "沙發", percentX: 0.7, percentY: 0.7),
-            ],
-          ),
-          const SizedBox(height: 20),
-          WrapperLabelImage(
-            width: 300,
-            imageUrl:
-                "https://cp4.100.com.tw/images/works/202505/16/api_2012032_1747362772_TBECTIDzJt.jpg!10309t1500-v2.jpg",
-            labels: [
-              LabelItem(name: "吊頂燈帶", percentX: 0.3, percentY: 0.3),
-              LabelItem(name: "電視墻", percentX: 0.4, percentY: 0.5),
-              LabelItem(name: "沙發", percentX: 0.7, percentY: 0.7),
-            ],
-          ),
-          const SizedBox(height: 20),
-          WrapperLabelImage(
-            width: 300,
-            height: 300,
-            imageUrl:
-                "https://cp4.100.com.tw/images/works/202505/16/api_2012032_1747362772_TBECTIDzJt.jpg!10309t1500-v2.jpg",
-            labels: [
-              LabelItem(name: "吊頂燈帶", percentX: 0.3, percentY: 0.3),
-              LabelItem(name: "電視墻", percentX: 0.4, percentY: 0.5),
-              LabelItem(name: "沙發", percentX: 0.7, percentY: 0.7),
-            ],
-          ),
-          const SizedBox(height: 20),
-          WrapperLabelImage(
-            imageUrl:
-                "https://cp4.100.com.tw/images/works/202301/03/api_2114251_1672736564_sk5rc5Iy49.jpg!t1000.webp",
-            labels: [
-              LabelItem(name: "壁櫃", percentX: 0.53, percentY: 0.47),
-              LabelItem(name: "冰箱", percentX: 0.69, percentY: 0.6),
-              LabelItem(name: "燃氣灶", percentX: 0.2, percentY: 0.8),
-            ],
-          ),
-          const SizedBox(height: 20),
-          WrapperLabelImage(
-            width: 200,
-            imageUrl:
-                "https://cp4.100.com.tw/images/works/202301/03/api_2114251_1672736564_sk5rc5Iy49.jpg!t1000.webp",
-            labels: [
-              LabelItem(name: "壁櫃", percentX: 0.53, percentY: 0.47),
-              LabelItem(name: "冰箱", percentX: 0.69, percentY: 0.6),
-              LabelItem(name: "燃氣灶", percentX: 0.2, percentY: 0.8),
-            ],
-          ),
-          const SizedBox(height: 20),
-          WrapperLabelImage(
-            width: 200,
-            height: 200,
-            imageUrl:
-                "https://cp4.100.com.tw/images/works/202301/03/api_2114251_1672736564_sk5rc5Iy49.jpg!t1000.webp",
-            labels: [
-              LabelItem(name: "壁櫃", percentX: 0.53, percentY: 0.47),
-              LabelItem(name: "冰箱", percentX: 0.69, percentY: 0.6),
-              LabelItem(name: "燃氣灶", percentX: 0.2, percentY: 0.8),
-            ],
-          ),
-          const SizedBox(height: 100),
-        ]),
+        child: Column(children: widgets),
       ),
     );
   }
+}
+
+class ImageBlock {
+  final String image;
+  final BoxFit fit;
+  final List<LabelItem> labels;
+
+  ImageBlock({
+    required this.image,
+    required this.fit,
+    required this.labels,
+  });
 }
 
 class LabelItem {
@@ -212,6 +232,7 @@ class _WrapperLabelImageState extends State<WrapperLabelImage> {
       if (listener != null) {
         imageStream.removeListener(listener);
       }
+      // Get the real image size
       Size imageSize = Size(
         imageInfo.image.width.toDouble(),
         imageInfo.image.height.toDouble(),
@@ -219,10 +240,29 @@ class _WrapperLabelImageState extends State<WrapperLabelImage> {
       _isLoading = false;
       setState(() {});
       WidgetsBinding.instance.addPostFrameCallback((_) {
+        // Get the bound size
         Size boundSize = context.size ?? Size.zero;
-        if (widget.fit == BoxFit.cover) {
+        if (widget.fit == BoxFit.contain) {
           double imageAspectRatio = imageSize.width / imageSize.height;
           double boundAspectRatio = boundSize.width / boundSize.height;
+          if (imageAspectRatio < boundAspectRatio) {
+            imageSize = Size(
+              boundSize.height / imageSize.height * imageSize.width,
+              boundSize.height,
+            );
+          } else {
+            imageSize = Size(
+              boundSize.width,
+              boundSize.width / imageSize.width * imageSize.height,
+            );
+          }
+        } else {
+          // BoxFit.cover
+          // Calculate the image size that showing in the bound
+          double imageAspectRatio = imageSize.width / imageSize.height;
+          double boundAspectRatio = boundSize.width / boundSize.height;
+          // If the image aspect ratio is less than the bound aspect ratio, the image will be scaled to fit the width of the bound
+          // Otherwise, the image will be scaled to fit the height of the bound
           if (imageAspectRatio < boundAspectRatio) {
             imageSize = Size(
               boundSize.width,
